e747262ce73887723dc0e26a9845e8522d8e1ab7
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-25 15:04:01 -0700

    Completed The functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -9,6 +9,7 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     size ++;
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
@@ -16,6 +17,16 @@ var Queue = function() {
     if (size < 0) {
       size = 0;
     }
+    var itemToRemove = storage['1'];
+    delete storage['1'];
+    var index = 1;
+    var newStorage = {};
+    for (var key in storage) {
+      newStorage[index] = storage[key];
+    }
+    console.log(storage, newStorage);
+    storage = newStorage;
+    return itemToRemove;  
   };
 
   someInstance.size = function() {
