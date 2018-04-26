a28ae832edaecf3121af77f18bf89e5a1813e27b
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-26 00:08:54 -0700

    Passed all pseudoclassical stack tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,13 +1,25 @@
 var Stack = function() {
  this.sizeOf = 0;
+ this.storage = {};
 };
 
 Stack.prototype.size = function() {
 	return this.sizeOf;
 }
 
-Stack.prototype.push = function() {
+Stack.prototype.push = function(value) {
 	this.sizeOf++;
+	this.storage[this.sizeOf] = value;
+}
+
+Stack.prototype.pop = function() {
+	var itemToRemvoe = this.storage[this.sizeOf];
+	delete this.storage[this.sizeOf]
+	this.sizeOf--;
+	if (this.sizeOf < 0) {
+		this.sizeOf = 0;
+	}
+	return itemToRemvoe;
 }
 
 
