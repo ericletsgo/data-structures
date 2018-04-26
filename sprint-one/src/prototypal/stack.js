20da4fe3b7dd21e80e22fda6c8a4dd7328171dab
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-25 18:22:53 -0700

    Completed prototypal stack tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
+  someInstance = Object.create(stackMethods);
   someInstance.sizeOf = 0;
-  _.extend(someInstance, stackMethods);
   return someInstance;
 };
 
 var stackMethods = {
-	size: function() {
-		return this.sizeOf;
-	},
 	push: function(value) {
 		this.sizeOf++;
+		this[this.sizeOf] = value;
+	},
+	pop: function() {
+		var itemToRemove = this[this.sizeOf];
+		delete this[this.sizeOf];
+		this.sizeOf--;
+		if (this.sizeOf < 0) {
+			this.sizeOf = 0;
+		}
+		return itemToRemove;
+	},
+	size: function() {
+		return this.sizeOf;
 	}
 };
-
 
