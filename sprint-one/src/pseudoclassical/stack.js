f6628ef5b1ac7bc8457e532db813abd60a74c488
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-25 23:47:42 -0700

    Passed first 2 tests of pseudoclassical suite

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,13 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+ this.sizeOf = 0;
 };
+
+Stack.prototype.size = function() {
+	return this.sizeOf;
+}
+
+Stack.prototype.push = function() {
+	this.sizeOf++;
+}
 
 
