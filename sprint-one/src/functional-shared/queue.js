6ed086dbe2e4e09601a57919f679871a4ecb6e2b
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-25 18:05:15 -0700

    Passed first two tests of prototypal stacks

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,7 @@
 var Queue = function() {
   var someInstance = {};
   someInstance.sizeOf = 0;
+  someInstance.storage = {};
   someInstance.size = queueMethods.getSize;
   someInstance.enqueue = queueMethods.enqueue;
   someInstance.dequeue = queueMethods.dequeue;
@@ -13,10 +14,23 @@ var queueMethods = {
 	},
 	enqueue: function(value) {
 		this.sizeOf++;
-		this[this.sizeOf] = value;
+		this.storage[this.sizeOf] = value;
 	},
 	dequeue: function() {
 		this.sizeOf--;
+		if (this.sizeOf < 0) {
+			this.sizeOf = 0;
+		}
+		var itemToRemove = this.storage['1'];
+		delete this.storage['1'];
+		var index = 1;
+		var newStorage = {};
+		for (var key in this.storage) {
+			newStorage[index] = this.storage[key]
+			index++;
+		}
+		this.storage = newStorage;
+		return itemToRemove;
 	}
 };
 
