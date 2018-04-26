ffe99097a7d2d20a57665b0e018dd716f6a6c09a
Daniel Olson <danielolsonah.school@gmail.com>
2018-04-25 17:14:51 -0700

    Passed first four tests on functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,12 +2,21 @@ var Queue = function() {
   var someInstance = {};
   someInstance.sizeOf = 0;
   someInstance.size = queueMethods.getSize;
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
   return someInstance;
 };
 
 var queueMethods = {
 	getSize: function() {
 		return this.sizeOf;
+	},
+	enqueue: function(value) {
+		this.sizeOf++;
+		this[this.sizeOf] = value;
+	},
+	dequeue: function() {
+		this.sizeOf--;
 	}
 };
 
