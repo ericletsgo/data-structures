9ab1fac1b60d74809141ba1cc25e0d92179b17d1
ericletsgo <ericletsgo@gmail.com>
2018-04-27 10:55:35 -0700

    Added time complexity notes for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -35,4 +35,6 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild is a constant time operation
+ * contains is a linear time operation
  */
